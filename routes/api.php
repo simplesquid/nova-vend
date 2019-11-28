4a51b7415a5c2d399d0f475b4ef5f1bdd633c032
Matthew Poulter <github@matthewpoulter.co.za>
2019-11-28 09:34:09 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Support\Vend\VendTokenManager;
+use SimpleSquid\LaravelVend\VendTokenManager;
 
 /*
 |--------------------------------------------------------------------------
